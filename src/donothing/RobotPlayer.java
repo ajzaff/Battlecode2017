12b1810185b6b4af6329c073887ccd15efd224a3
ajzaff <azco21@cox.net>
2017-01-10 13:27:28 -0500

    Remove GameActionException from donothing player
    Signed-off-by: ajzaff <azco21@cox.net>

--- src/donothing/RobotPlayer.java
+++ src/donothing/RobotPlayer.java
@@ -1,12 +1,11 @@
 package donothing;
 
-import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 
 public strictfp class RobotPlayer {
 
     @SuppressWarnings({"unused", "InfiniteLoopStatement", "StatementWithEmptyBody"})
-    public static void run(RobotController rc) throws GameActionException {
+    public static void run(RobotController rc) {
         while(true);
     }
 
