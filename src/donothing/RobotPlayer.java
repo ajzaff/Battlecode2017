fcddfbabddb3df335da77a31b3871852b854c331
ajzaff <azco21@cox.net>
2017-01-10 12:33:48 -0500

    Add donothing player
    Signed-off-by: ajzaff <azco21@cox.net>

---
+++ src/donothing/RobotPlayer.java
@@ -0,0 +1,13 @@
+package donothing;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public strictfp class RobotPlayer {
+
+    @SuppressWarnings({"unused", "InfiniteLoopStatement", "StatementWithEmptyBody"})
+    public static void run(RobotController rc) throws GameActionException {
+        while(true);
+    }
+
+}
