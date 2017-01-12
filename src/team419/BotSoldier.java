af85c428d12611a494834861276d5d2f47c68aca
ajzaff <azco21@cox.net>
2017-01-12 16:49:35 -0500

    Add basic stub micro behavior to units
    Signed-off-by: ajzaff <azco21@cox.net>

--- src/team419/BotSoldier.java
+++ src/team419/BotSoldier.java
@@ -1,17 +1,20 @@
 package team419;
 
-import battlecode.common.Clock;
-import battlecode.common.GameActionException;
-import battlecode.common.TreeInfo;
+import battlecode.common.*;
 
+import static battlecode.common.GameConstants.LUMBERJACK_STRIKE_RADIUS;
+import static battlecode.common.RobotType.LUMBERJACK;
 import static battlecode.common.Team.NEUTRAL;
 
 final strictfp class BotSoldier extends Navigation {
+
+    private static int rotateRandomness;
 
     @SuppressWarnings("InfiniteLoopStatement")
     static void loop() {
         FastMath.initRand(rc);
         exploreDir = Navigation.getRandomDirection();
+        rotateRandomness = FastMath.rand() % 2 == 0? 1 : -1;
 
         while (true) {
             int begin = rc.getRoundNum();
@@ -32,10 +35,25 @@ final strictfp class BotSoldier extends Navigation {
 
     private static void act() throws GameActionException {
         GameState.senseNearbyTrees();
+        GameState.senseNearbyEnemies();
         tryShakeNearbyTree();
-        for (int i = 0; i < 6 && !Navigation.tryMoveInDirection(exploreDir); i++) {
-            exploreDir = exploreDir.rotateRightRads(EIGHTH_TURN);
+        tryMicro();
+        tryExploreMap();
+    }
+
+    private static boolean tryExploreMap() throws GameActionException {
+        if (rc.hasMoved())
+            return false;
+
+        for (int i = 0; i < 6; i++) {
+            if (rc.canMove(exploreDir)) {
+                rc.move(exploreDir);
+                return true;
+            }
+            exploreDir = exploreDir.rotateRightRads(rotateRandomness * EIGHTH_TURN);
         }
+
+        return false;
     }
 
     private static boolean tryShakeNearbyTree() throws GameActionException {
@@ -53,5 +71,103 @@ final strictfp class BotSoldier extends Navigation {
             return false;
         rc.shake(bestTree.location);
         return true;
+    }
+
+    private static boolean tryMicro() throws GameActionException {
+
+        if (!rc.hasAttacked()) {
+
+            // Try to shoot
+            if (nearbyEnemies.length > 0) {
+
+                RobotInfo bestTarget = null;
+                double bestScore = -99999;
+                float distance = 0;
+
+                for (RobotInfo r : nearbyEnemies) {
+                    double score = targetScore(r);
+                    if (score > bestScore) {
+                        bestTarget = r;
+                        bestScore = score;
+                        distance = myLoc.distanceTo(r.location);
+                    }
+                }
+
+                if (bestTarget != null)
+                    if (distance < 4 && rc.canFirePentadShot())
+                        rc.firePentadShot(myLoc.directionTo(bestTarget.location));
+                    else if (distance < 9 && rc.canFireTriadShot())
+                        rc.fireTriadShot(myLoc.directionTo(bestTarget.location));
+                    else if (rc.canFireSingleShot())
+                        rc.fireSingleShot(myLoc.directionTo(bestTarget.location));
+            }
+        }
+
+        if (!rc.hasMoved()) {
+
+            MapLocation enemyLoc = myLoc;
+            Direction dir = null;
+
+            // Try to pick a fight with a nearby enemy
+            // Or run away if we've already attacked this turn
+            if (nearbyEnemies.length > 0) {
+                for (RobotInfo r : nearbyEnemies) {
+                    enemyLoc = enemyLoc.add(myLoc.directionTo(r.location));
+                }
+                if (rc.hasAttacked()) {
+                    dir = enemyLoc.directionTo(myLoc);
+                } else {
+                    dir = myLoc.directionTo(enemyLoc);
+                }
+                if (Navigation.tryMoveInDirection(dir)) {
+                    return true;
+                }
+            }
+
+            RobotInfo[] friends = GameState.senseNearbyRobots(1 + LUMBERJACK_STRIKE_RADIUS, myTeam);
+            MapLocation friendLoc = myLoc;
+
+            // Try to move away from friendly lumberjack strike radius
+            for (RobotInfo r : friends)
+                if (r.type == LUMBERJACK)
+                    friendLoc = friendLoc.add(myLoc.directionTo(friendLoc));
+
+            dir = friendLoc.directionTo(myLoc);
+            if (dir != null) {
+                Navigation.tryMoveInDirection(dir);
+            }
+        }
+
+        return true;
+    }
+
+    private static double targetScore(RobotInfo r) {
+        double v;
+
+        switch (r.type) {
+        case ARCHON:
+            v = 2 * (1 - r.health / r.type.maxHealth);
+            break;
+        case SOLDIER:
+            v = .9 * (1 - r.health / r.type.maxHealth);
+            break;
+        case LUMBERJACK:
+            v = .8 * (1 - r.health / r.type.maxHealth);
+            break;
+        case TANK:
+            v = .7 * (1 - r.health / r.type.maxHealth);
+            break;
+        case GARDENER:
+            v = .2 * (1 - r.health / r.type.maxHealth);
+            break;
+        case SCOUT:
+            v = .1 * (1 - r.health / r.type.maxHealth);
+            break;
+        default:
+            v = 0;
+        }
+        if (r.team == myTeam)
+            v *= -1;
+        return v;
     }
 }
